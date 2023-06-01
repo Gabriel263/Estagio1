c989fffbb71992a57c923970b5640c0d4f7ec03b
Gabriel263 <blum.santos@unemat.br>
2023-06-01 19:00:44 -0300

    correção de erro cadastrar.php

--- PHP/cadastrar.php
+++ PHP/cadastrar.php
@@ -93,6 +93,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         exit();
     } catch (PDOException $e) {
         echo "Erro ao cadastrar o usuário: " . $e->getMessage();
+        exit();
     }
 }
 ?>
